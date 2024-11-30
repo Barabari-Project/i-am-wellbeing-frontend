69ae550940eef852ebf7d58645e08ad5f69ee5b5
juber <juberk1010@gmail.com>
2024-11-30 10:11:57 +0530

    fext: making ticp page

--- src/pages/Ticp.jsx
+++ src/pages/Ticp.jsx
@@ -2,8 +2,93 @@ import React from 'react'
 
 const Ticp = () => {
   return (
-    <div>Ticp</div>
-  )
+    <div className='container m-auto'>
+      <div className='about-us'>
+        <h2 className='title'>About us</h2>
+        <p>
+          Trauma is the most unaddressed and unseen epidemic in today’s time.
+          Seven out of ten children in India experience some form of adversity
+          at an early age. If left unaddressed, these painful experiences can
+          haunt them for the rest of their lives, deeply impacting their mental,
+          emotional, and physical wellbeing.
+        </p>
+
+        <p>
+          I Am Wellbeing (Nairatmya Foundation) is a mental health not-for-
+          profit organization dedicated to supporting such young lives
+          struggling with adverse childhood experiences and building an
+          ecosystem of trusted, available, trained adults. We believe in the
+          power of compassion, connection, and care to develop a healthy and
+          happy nation.
+        </p>
+        <h2 className='title'>Rationale</h2>
+        <p>
+          While the last decade has highlighted the significant need for mental
+          health interventions, it has also exposed the scarcity of trained
+          professionals compared to the staggeringly high number of people in
+          need of mental health care and support in India. With each passing day
+          we get to witness a new statistic or data related to increasingly high
+          number of deaths by suicide, depression rate, severe mental health and
+          physical health concerns like chronic illnesses, heart problems, and
+          autoimmune diseases.
+        </p>
+        <p>
+          The key to mitigating the impact of the perpetually increasing
+          wellbeing concerns in our country is to look beyond the presenting
+          symptoms of an individual and work towards the root cause through a
+          holistic and trauma- informed outlook.
+        </p>
+        <p>
+          We strive to fulfill this unmet need of quality mental health care by
+          building the capacity of professionals towards adapting a trauma-
+          informed approach that is inclusive, contextualised, and
+          de-stigmatising. To bring this shift in managing mental health
+          concerns, we have designed a 6 months long coursework on trauma-
+          informed care practices. Experts from the field will support the
+          participants to build a trauma-informed lens for intervention
+          planning.
+        </p>
+
+        <p>
+          The course will create an understanding of evidence-based holistic
+          tools used to process deep-rooted trauma.
+        </p>
+      </div>
+      <div className='Activities'>
+        <h2 className='title'>Course Activities & Duration</h2>
+        <p>
+          The course will encompass various activities like learning sessions,
+          group discussions, assignments, case conceptualisation, roleplays &
+          supervision. There are a total of six modules to be covered over a
+          period of two months. Upon completion of each module, the participants
+          will submit graded assignments. The end of the theory sessions will
+          embark on the beginning of four months of trauma-informed supervision
+          with experts from the field. As the supervision commences,
+          participants will attend a viva and submit case studies. The intensive
+          process is designed to ensure the learning and application of trauma-
+          informed care practices successfully.
+        </p>
+
+        <div className='activit-list'>
+          <p>
+            Duration: 6 Months Theory sessions: 15 Assignments: 6 Case
+            submissions: 5 Supervision sessions: 8
+          </p>
+          <img src='https://www.iamwellbeing.org/imgs/ticp4.png' alt='' />
+        </div>
+
+        <img src='https://www.iamwellbeing.org/imgs/2.png' alt='' />
+        <img src='https://www.iamwellbeing.org/imgs/2a.png' alt='' />
+      </div>
+      <div className='beyond'>
+        <p>
+          Successful completion of level 1 will qualify you to apply for Level 2
+          This level is a more specialized coursework, which will focus upon
+          topics like:
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Ticp
