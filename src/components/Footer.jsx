fca9fded2913d5da33019cda20e5f66f3bfb624f
Deepakgit config --global user.email d84255411@gamil.comgit config --global user.name Deepak <d84255411@gamil.com>
2025-03-07 22:06:23 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,19 +11,38 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className='footer bg-black text-white lg:pt-5 pb-5 '>
-      <div className='max-w-5xl m-auto flex lg:flex-row flex-col lg:gap-10 items-start  p-10 lg:p-0'>
+      {/* <div className='max-w-5xl m-auto flex lg:flex-row flex-col lg:gap-10 items-start  p-10 lg:p-0'> */}
        
-        <div className='row align-items-baseline sm:flex sm:justify-between w-full '>
+        <div className='row align-items-baseline sm:flex sm:justify-around w-full '>
           <div className='col-md-6 text-center'>
             <p className='footer-copyright text-white mt-1 text-xl max-md:text-lg '>
               ©2025 Nairatmya Foundation. All rights reserved.
             </p>
           </div>
-          <div className='col-md-6 text-center'>
-            <p className='footer-copyright text-white mt-1 text-xl max-md:text-lg'>Developed by The Barabari Collective.</p>
+          <div className="w-full lg:w-1/2 mt-4 lg:mt-0 text-center lg:text-center">
+            <p className="text-sm text-gray-400">
+              Grateful to{" "}
+              <a
+                href="https://www.questionpro.com/"
+                className="text-gray-400 underline hover:text-gray-300 transition-colors"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                QuestionPro
+              </a>{" "}
+              for their generous provision of{" "}
+              <a
+                href="https://www.questionpro.com/survey-templates/"
+                className="text-gray-400 underline hover:text-gray-300 transition-colors"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                free survey templates
+              </a>, empowering our nonprofit initiatives.
+            </p>
           </div>
         </div>
-      </div>
+      {/* </div> */}
     </footer>
   );
 };
