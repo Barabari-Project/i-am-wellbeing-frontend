8222bdc6c7ba0ca8192565e0d473a4591d39fb15
Rylen Lobo <loborylen98334@gmail.com>
2025-06-27 09:19:10 +0530

    Format and clean up Vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  	base: '/',
-	plugins: [react()],
-	assetsInclude: ['**/*.JPG', '**/*.PNG', '**/*.GIF', '**/*.WEBP', '**/*.jpeg', '**/*.jpg'],
-	allowedHosts: ['*'] // Allows all hosts (not recommended for production)
-
-})
+  base: "/",
+  plugins: [react()],
+  allowedHosts: ["*"], // Allows all hosts (not recommended for production)
+});
