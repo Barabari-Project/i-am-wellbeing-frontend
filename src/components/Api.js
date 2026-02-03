7ce189896fe7f2f670638c9d35283936bae11741
wsevolves <wsevolves@gmail.com>
2026-02-03 15:29:50 +0530

    Contact Form Test

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,16 +1,45 @@
 import axios from "axios";
 
-const BASE_URL = "https://i-am-wellbeing-backend.onrender.com/wellBeing/api";
+// const BASE_URL = "https://i-am-wellbeing-backend.onrender.com/wellBeing/api/";
 
-export const sendMessage = async (data) => {
-    try {
-        const response = await axios.post(`${BASE_URL}/message`, data);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
-}
+// export const sendMessage = async (formData) => {
+//   try {
+//     const response = await fetch("http://localhost/i-am-wellbeing-frontend/sendMessage.php", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(formData),
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error("API Error:", error);
+//     throw error;
+//   }
+// };
+export const sendMessage = async (formData) => {
+  try {
+    const response = await fetch(
+      `${window.location.origin}/sendMessage.php`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("API Error:", error);
+    throw error;
+  }
+};
+
+
 
 export const sendAccountDetails = async (data) => {
     try {
