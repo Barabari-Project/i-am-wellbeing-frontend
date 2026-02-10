478e29b844aec81aede60597bb56146317224c25
wsevolves <165700713+wsevolves@users.noreply.github.com>
2026-02-10 18:58:47 +0530

    Add Needs in API
    Added a new sendNeeds function to handle needs submissions via EmailJS.

--- src/components/Api.js
+++ src/components/Api.js
@@ -37,6 +37,8 @@ import emailjs from "emailjs-com";
 const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
+const NEEDS_TEMPLATE_ID = import.meta.env.VITE_NEED_TEMPLATE_ID;
+
 
 // ✅ CONTACT FORM (EmailJS – NO BACKEND)
 export const sendMessage = async (formData) => {
@@ -77,13 +79,38 @@ export const sendAccountDetails = async (data) => {
         
     }
 }
+export const sendNeeds = async (formData) => {
+  try {
+    const result = await emailjs.send(
+      SERVICE_ID,
+      NEEDS_TEMPLATE_ID,
+      {
+        name: formData.nameneeds,
+        email: formData.emailneeds,
+        need: formData.need,
+      },
+      PUBLIC_KEY
+    );
 
-export const sendNeeds = async (data) => {
-    try {
-        const response = await axios.post(`${BASE_URL}/needs`, data);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
+    return {
+      status: "success",
+      message: "Needs submitted successfully",
+    };
+  } catch (error) {
+    console.error("EmailJS Error:", error);
+    return {
+      status: "error",
+      message: "Failed to submit needs",
+    };
+  }
+};
+
+// export const sendNeeds = async (data) => {
+//     try {
+//         const response = await axios.post(`${BASE_URL}/needs`, data);
+//         return response.data;
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     }
+// }
